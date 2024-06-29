365bee8fc52cec7cd56acbd4b8e1648243458cec
N-H-U-A-C <presenteisme.pb@gmail.com>
2024-06-29 23:21:12 +0200

    chore: delete hibernate.cfg.xml file and add addAnnotatedClasses method to SessionFactorySingleton class

--- src/main/java/dev/cb/store/persistence/SessionFactorySingleton.java
+++ src/main/java/dev/cb/store/persistence/SessionFactorySingleton.java
@@ -1,17 +1,28 @@
 package dev.cb.store.persistence;
 
+import dev.cb.store.business.model.ElectronicItem;
+import dev.cb.store.business.model.FashionItem;
+import dev.cb.store.business.model.FoodItem;
+import dev.cb.store.business.model.Item;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+// TODO check try-with-resources
+// TODO check synchronized
+// TODO refactor with an enum
 public class SessionFactorySingleton {
     private static StandardServiceRegistry standardServiceRegistry;
     private static SessionFactory sessionFactory;
 
     private SessionFactorySingleton() {
-        standardServiceRegistry = new StandardServiceRegistryBuilder().configure().build();
-        sessionFactory = new MetadataSources(standardServiceRegistry).buildMetadata().buildSessionFactory();
+        standardServiceRegistry = new StandardServiceRegistryBuilder()
+                .build();
+        sessionFactory = new MetadataSources(standardServiceRegistry)
+                .addAnnotatedClasses(Item.class, ElectronicItem.class, FashionItem.class, FoodItem.class)
+                .buildMetadata()
+                .buildSessionFactory();
     }
 
     public static synchronized SessionFactory getSessionFactory() {
