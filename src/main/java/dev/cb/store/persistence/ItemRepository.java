7824b6476bb5cde160ff445d1594564d4b00de06
N-H-U-A-C <presenteisme.pb@gmail.com>
2024-06-30 02:11:20 +0200

    refactor: use inTransaction method in save, findById and findAll methods for ItemRepository class

--- src/main/java/dev/cb/store/persistence/ItemRepository.java
+++ src/main/java/dev/cb/store/persistence/ItemRepository.java
@@ -4,8 +4,6 @@ import dev.cb.store.business.model.Item;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import javax.persistence.TypedQuery;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,53 +16,17 @@ public abstract class ItemRepository {
         sessionFactory = SessionFactorySingleton.getSessionFactory();
     }
 
-    public Item save(Item item) {
-        try {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            session.save(item);
-            session.getTransaction().commit();
-            return item;
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            System.out.println(e.getMessage());
-            return null;
-        } finally {
-            session.close();
-        }
+    public void save(Item item) {
+        sessionFactory.inTransaction(session -> session.persist(item));
     }
 
     public Optional<Item> findById(long id) {
-        try {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            Optional<Item> optionalItem = Optional.ofNullable(session.get(Item.class, id));
-            session.getTransaction().commit();
-            return optionalItem;
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            System.out.println(e.getMessage());
-            return Optional.empty();
-        } finally {
-            session.close();
-        }
+        return sessionFactory.fromTransaction(session -> Optional.ofNullable(session.find(Item.class, id)));
     }
 
     public List<Item> findAll() {
-
-        try {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            TypedQuery<Item> query = session.createQuery("from Item", Item.class);
-            List<Item> items = query.getResultList();
-            session.getTransaction().commit();
-            return items;
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            System.out.println(e.getMessage());
-            return new ArrayList<>();
-        } finally {
-            session.close();
-        }
+        return sessionFactory.fromTransaction(session ->
+                session.createSelectionQuery("from Item", Item.class)
+                        .getResultList());
     }
 }
